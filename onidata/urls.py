8bb71ccc4b0c9b0771bd8c219d5acb59b7cd0fa5
Victor Augusto <victor.augusto.soliveira@gmail.com>
2023-05-19 14:36:13 -0300

    fix django debug toolbar - correct if

--- onidata/urls.py
+++ onidata/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from onidata.settings import DEBUG
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
